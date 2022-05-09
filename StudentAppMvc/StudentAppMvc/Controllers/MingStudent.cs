44d5d39f985dcc6c50bd6313e3499195394f1604
Minh Nguyen <minh.nguyen@episerver.com>
2022-05-09 09:51:12 +0700

    Add MingStudent

---
+++ StudentAppMvc/StudentAppMvc/Controllers/MingStudent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentAppMvc.Controllers
+{
+    public class MingStudent : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
