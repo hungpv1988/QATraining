39d9bb1f9e068547baf804199896d833f2be9df9
Minh Nguyen <minh.nguyen@episerver.com>
2022-05-11 12:48:56 +0700

    add some validation

--- StudentAppMvc/StudentAppMvc/Models/Student.cs
+++ StudentAppMvc/StudentAppMvc/Models/Student.cs
@@ -1,20 +1,30 @@
-﻿namespace StudentAppMvc.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentAppMvc.Models
 {
     public class Student
     {
      
         public int Id { get; set; }
-        
+
+        [Display(Name = "Student Name")]
+        [StringLength(60, MinimumLength = 1)]
+        [Required]
         public string Name { get; set; }
 
         public string Description { get; set; }
 
+        [Display(Name = "Date Of Birth")]
+        [DataType(DataType.Date)]
         public int DateOfBirth { get; set; }
 
         public bool Gender { get; set; }
 
+        [RegularExpression(@"^([\w\.\-]+)@hut.edu$")]
+        [Required]
         public string Email { get; set; }
 
+        [StringLength(5)]
         public string StudentAccount { get; set; }
     }
 }
